11f106b10ef6b294e41013e6c796e0c496f580a4
Myoung <mhbae93@gmail.com>
2021-03-28 00:54:51 +0000

    hotfix:initialcards

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,6 @@
 //Imports
 import { togglePopup, escapePopup, closeClick } from "./utils.js";
 import FormValidator from "./FormValidator.js";
-import initialCards from "./initialCards.js";
 import Card from "./Card.js";
 
 //VARIABLES
@@ -14,6 +13,34 @@ const validationObject = {
   inputErrorClass: "popup__input_type_error",
   errorClass: "popup__error_visible",
 };
+
+//elements array
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+];
 
 //Popups
 const popupAdd = document.querySelector(".popup_type_add");
